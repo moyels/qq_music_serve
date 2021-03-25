43daf7cc4a71b2e92d432749072f31d40fedeee3
moyel <1010404561@qq.comn>
2021-03-25 18:51:23 +0800

    request rank

--- pages/api/rank.js
+++ pages/api/rank.js
@@ -6,7 +6,17 @@ const HEADERS = {
     'referer': "https://i.y.qq.com/",
 }
 
+const request = require('request-promise');
 
-export default (req, res) => {
-    res.status(200).json({result: "sb lianglu"})
+export default async (req, res) => {
+    const url = `https://u.y.qq.com/cgi-bin/musicu.fcg?_=${+new Date()}&data={"comm":{"g_tk":1775699468,"uin":2313970630,"format":"json","inCharset":"utf-8","outCharset":"utf-8","notice":0,"platform":"h5","needNewCode":1,"ct":23,"cv":0},"topList":{"module":"musicToplist.ToplistInfoServer","method":"GetAll","param":{}}}`
+    try {
+        res.json(await request({
+            url: url,
+            json: true,
+            headers: HEADERS
+        }))
+    } catch (e) {
+        res.json({error: e.massage})
+    }
 }
